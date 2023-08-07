561280e19ecd069d450fd3d71ca6e48060394ccd
Helen Esterman <helenesterman99@gmail.com>
2023-08-07 12:52:35 -0400

    practiced fetch() command with random fox images

---
+++ scripts/main.js
@@ -0,0 +1,9 @@
+fetch("https://randomfox.ca/floof/")
+    .then(response => response.json())
+    .then(
+        (convertedData) => {
+            const foxImgElement = document.querySelector("#fox")
+            foxImgElement.src = convertedData.image
+        }
+        
+    )
